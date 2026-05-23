1a86ea1842a4b789444e8b803fc3f40942e2f6f2
Zzw <2865761465@qq.com>
2026-05-23 19:29:05 +0800

    11

---
+++ 控制流程/控制流程作业.py
@@ -0,0 +1,49 @@
+"""
+"""
+"""
+1、使用for循环，将列表 a =[1,4,5,100,5,78,5]筛选出除了5以外的其他元素并存入一个新的列表当中
+"""
+# a =[1,4,5,100,5,78,5]
+# result = []
+# for num in a:
+#     if num == 5:
+#         continue
+#     result.append(num)
+# print(result)
+
+# age = int(input("请输入年龄："))
+# if age > 64:
+#     print("可以退休")
+# else:
+#     print("接着沉淀")
+
+
+# money = int(input("请输入账户余额："))
+# if money >=5000:
+#     print("可以旅游")
+# elif money >=2000:
+#     print("可以购物")
+
+
+# age = int(input("请输入年纪："))
+# ID = input("是否为学生：")
+# if age <= 18 and ID == "学生":
+#     print("可以获得学生优惠")
+# else:
+#     print("无法获得学生优惠")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
