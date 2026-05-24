94d3b8667ec5aa2f1c85526078d2dff5f890a5cb
Zzw <2865761465@qq.com>
2026-05-24 11:03:07 +0800

    作业补充

--- 控制流程/控制流程作业.py
+++ 控制流程/控制流程作业.py
@@ -1,15 +1,25 @@
 """
 """
 """
-1、使用for循环，将列表 a =[1,4,5,100,5,78,5]筛选出除了5以外的其他元素并存入一个新的列表当中
+1、使用for循环,将列表 a =[1,4,5,100,5,78,5]筛选出除了5以外的其他元素并存入一个新的列表当中
 """
 # a =[1,4,5,100,5,78,5]
-# result = []
+# result = []    # 列表放在循环的外面，若写在循环中，每次循环都会创建一个新的列表，导致之前的结果丢失
 # for num in a:
 #     if num == 5:
 #         continue
 #     result.append(num)
 # print(result)
+
+
+# a =[1,4,5,100,5,78,5]
+# result = []
+# for num in a:
+#     if num != 5:     #  != 5 代表不等于5
+#         result.append(num)
+# print(result)
+
+
 
 # age = int(input("请输入年龄："))
 # if age > 64:
@@ -34,15 +44,37 @@
 
 
 
+"""
+循环题目
+"""
+# 3、已知一个数字列表 list = [59,23,56,78,90,234,64,12,9],求列表中所有数据和
+# list = [59,23,56,78,90,234,64,12,9]
+# total = 0
+# for num in list:
+#     total += num
+# print(total)    
 
 
+# 4、控制台输入用户名，判断用户名是否合法（用户名长度6-10位即为合法，最多十次输入，满足条件结束
+# name = input("请输入用户名：")
+# count = 0
+# while count < 10:
+#     if 6 <= len(name) <= 10:
+#         print("用户名合法")
+#         break
+#     else:
+#         print("用户名不合法，请重新输入")
+#         name = input("请输入用户名：")
+#         count += 1
 
-
-
-
-
-
-
+# name = input("请输入用户名：")
+# for count in range(10):
+#     if 6 <= len(name) <= 10:
+#         print("可以使用")
+#         break
+#     else:
+#         print("重起")
+#         name = input("请输入用户名：")
 
 
 
