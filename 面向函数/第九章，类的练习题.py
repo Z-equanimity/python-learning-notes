8fbaa5a39e3333d58fc1020382b108be87c1b795
Zzw <2865761465@qq.com>
2026-06-05 11:10:32 +0800

    zz

--- 面向函数/第九章，类的练习题.py
+++ 面向函数/第九章，类的练习题.py
@@ -1,33 +1,33 @@
 # 练习9.1、9.2、9.4：餐馆
-# class Restaurant():
-#     def __init__(self,name,type):
-#         self.name = name
-#         self.type = type
-#         self.number_served = 0
-#     def describe_restaurant(self):
-#         print(f"{self.name},{self.type}")
-#     def open_restaurant(self):
-#         print("正在营业")
-#     def update_number_served(self,number):
-#         self.number_served = number        
-#     def read_number_served(self):
-#         print(f"就餐人数{self.number_served}")
-#     def increment_number_servrd(self,newing):
-#         if newing > 0:
-#             self.number_served += newing 
-#         else:
-#             print("今天生意不太行啊，下去沉淀")
-# name1_restaurant = Restaurant("江南","shuang")
-# name1_restaurant.describe_restaurant()
-# name1_restaurant.update_number_served(3)
-# name1_restaurant.read_number_served()
-# name1_restaurant.increment_number_servrd(8)
-# name1_restaurant.read_number_served()
-# name1_restaurant.open_restaurant()
-# name2_restaurant = Restaurant("henan","shuang")
-# name2_restaurant.describe_restaurant()
-# name3_restaurant = Restaurant("hebei","shuang")
-# name3_restaurant.describe_restaurant()
+class Restaurant():
+    def __init__(self,name,type):
+        self.name = name
+        self.type = type
+        self.number_served = 0
+    def describe_restaurant(self):
+        print(f"{self.name},{self.type}")
+    def open_restaurant(self):
+        print("正在营业")
+    def update_number_served(self,number):
+        self.number_served = number        
+    def read_number_served(self):
+        print(f"就餐人数{self.number_served}")
+    def increment_number_servrd(self,newing):
+        if newing > 0:
+            self.number_served += newing 
+        else:
+            print("今天生意不太行啊，下去沉淀")
+name1_restaurant = Restaurant("江南","shuang")
+name1_restaurant.describe_restaurant()
+name1_restaurant.update_number_served(3)
+name1_restaurant.read_number_served()
+name1_restaurant.increment_number_servrd(8)
+name1_restaurant.read_number_served()
+name1_restaurant.open_restaurant()
+name2_restaurant = Restaurant("henan","shuang")
+name2_restaurant.describe_restaurant()
+name3_restaurant = Restaurant("hebei","shuang")
+name3_restaurant.describe_restaurant()
 
 # 练习9.3、9.5
 class User():
